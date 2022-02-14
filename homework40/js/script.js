c1f4f0e9419d2d1c27b9d3000865a925dae88cbc
VladITGod <vorobiov.v90@gmail.com>
2022-02-14 19:51:27 +0200

    init

---
+++ homework40/js/script.js
@@ -0,0 +1,27 @@
+function cacheAbility () {
+
+    let cache = new Map()
+
+    return (func) => {
+
+        if (cache.size >= 10) {
+            const cacheArr = Array.from(cache)
+            cacheArr.splice(0, 1)
+            cache = new Map(cacheArr)
+        }
+
+        if (cache.has(func)) return `${cache.get(func)} - value is taken from cache`
+
+        cache.set(func, func)
+        return cache.get(func)
+    }
+}
+
+function sum (num1, num2) {
+    return num1 + num2
+}
+
+const cachedData = cacheAbility()
+
+
+
