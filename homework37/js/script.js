f6bf3c35170be4e3547657b2c400e65953edf66e
VladITGod <vorobiov.v90@gmail.com>
2022-04-11 17:29:26 +0300

    regExp updated

--- homework37/js/script.js
+++ homework37/js/script.js
@@ -1,5 +1,3 @@
-const regExp = /([^\s]+()|(".+"))@[*[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+]*/;
-
 const emailsArr = [
   `simple@example.ua`,
   `very.common@example.com`,
@@ -10,16 +8,27 @@ const emailsArr = [
   `x@example.com`,
   `example-indeed@strange-example.com`,
   `test/test@test.com`,
-  `example@s.example `,
+  `example@s.example`,
   `" "@example.org`,
   `"john..doe"@example.org`,
-  `mailhost!username@example.org `,
+  `mailhost!username@example.org`,
   `user%example.com@example.org`,
-  `user-@example.org `,
+  `user-@example.org`,
+  `mail@withoutdot`,
+  `mail@with@three@.js`,
+  `.user-@example.org`,
 ];
 
+const regExp =
+  /^(\"[^\ \"]{1,64}(?!.*\"\")\"|^(?!^\.)(?!.*\.@)(?!.*\.\.).{1,64})@(?!\-)(?!\d*\.)[a-zA-Z0-9\-]*\.[a-zA-Z0-9]*(?<!\-)$/;
+
 function validateEmail(arr) {
-  for (let i = 0; i < arr.length; i++) console.log(regExp.exec(arr[i])[0]);
+  for (let i = 0; i < arr.length; i++)
+    if (regExp.test(arr[i]) === true)
+      console.log(`${arr[i]} - Success validation`);
+    else {
+      console.log(`${arr[i]} - Failed validation`);
+    }
 }
 
 validateEmail(emailsArr);
