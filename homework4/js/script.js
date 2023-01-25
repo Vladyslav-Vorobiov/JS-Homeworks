240a5a8b18b5f3a84614ce3d79a383b9733611f4
VladITGod <vorobiov.v90@gmail.com>
2023-01-25 13:26:30 +0200

    init

--- homework4/js/script.js
+++ homework4/js/script.js
@@ -1,34 +1,34 @@
-let operand1 = +prompt("Please enter first number")
-let operator = prompt("Please choose and enter available operator (add, sub, mult, div)");
-let operand2 = +prompt("Please enter second number")
+const operand1 = +prompt("Please enter first number");
+const operator = prompt(
+  "Please choose and enter available operator (add, sub, mult, div)"
+);
+const operand2 = +prompt("Please enter second number");
 
-
-function add () {
-    let result = operand1 + operand2;
-    return result
+function add() {
+  let result = operand1 + operand2;
+  return result;
 }
 
-function sub () {
-    let result = operand1 - operand2;
-    return result
+function sub() {
+  let result = operand1 - operand2;
+  return result;
 }
 
-function mult () {
-    let result = operand1 * operand2;
-    return result
+function mult() {
+  let result = operand1 * operand2;
+  return result;
 }
 
-function div () {
-    let result = operand1 / operand2;
-    return result
+function div() {
+  let result = operand1 / operand2;
+  return result;
 }
 
-let math = {
-    add: `${operand1} + ${operand2} = ${add()}`,
-    sub: `${operand1} - ${operand2} = ${sub()}`,
-    mult: `${operand1} * ${operand2} = ${mult()}`,
-    div: `${operand1} / ${operand2} = ${div()}`,
-  };
+const math = {
+  add: `${operand1} + ${operand2} = ${add()}`,
+  sub: `${operand1} - ${operand2} = ${sub()}`,
+  mult: `${operand1} * ${operand2} = ${mult()}`,
+  div: `${operand1} / ${operand2} = ${div()}`,
+};
 
-
-  console.log(math[operator])
+console.log(math[operator]);
