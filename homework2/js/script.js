3182724c32a8158c122de55d4698833ed32b9369
VladITGod <vorobiov.v90@gmail.com>
2023-01-25 13:00:44 +0200

    init

--- homework2/js/script.js
+++ homework2/js/script.js
@@ -4,25 +4,25 @@ let op1 = +prompt("Enter first operand");
 let op2 = +prompt("Enter second operand");
 
 function summ() {
-    let result = op1 + op2;
-    console.log(`Result: ${op1} + ${op2} = ${result}`)
+  let result = op1 + op2;
+  console.log(`Result: ${op1} + ${op2} = ${result}`);
 }
 summ();
 
 function subtract() {
-    let result = op1 - op2;
-    console.log(`Result: ${op1} - ${op2} = ${result}`)
+  let result = op1 - op2;
+  console.log(`Result: ${op1} - ${op2} = ${result}`);
 }
 subtract();
 
 function multiply() {
-    let result = op1 * op2;
-    console.log(`Result: ${op1} * ${op2} = ${result}`)
+  let result = op1 * op2;
+  console.log(`Result: ${op1} * ${op2} = ${result}`);
 }
 multiply();
 
 function divide() {
-    let result = op1 / op2;
-    console.log(`Result: ${op1} / ${op2} = ${result}`)
+  let result = op1 / op2;
+  console.log(`Result: ${op1} / ${op2} = ${result}`);
 }
 divide();
--- homework40/js/script.js
+++ homework40/js/script.js
@@ -1,29 +1,25 @@
-'use strict'
+"use strict";
 
-function cacheAbility () {
+function cacheAbility() {
+  let cache = new Map();
 
-    let cache = new Map()
-
-    return (func) => {
-
-        if (cache.size >= 10) {
-            const cacheArr = Array.from(cache)
-            cacheArr.splice(0, 1)
-            cache = new Map(cacheArr)
-        }
-
-        if (cache.has(func)) return `${cache.get(func)} - value is taken from cache`
-
-        cache.set(func, func)
-        return cache.get(func)
+  return (func) => {
+    if (cache.size >= 10) {
+      const cacheArr = Array.from(cache);
+      cacheArr.splice(0, 1);
+      cache = new Map(cacheArr);
     }
+
+    if (cache.has(func))
+      return `${cache.get(func)} - value is taken from cache`;
+
+    cache.set(func, func);
+    return cache.get(func);
+  };
 }
 
-function sum (num1, num2) {
-    return num1 + num2
+function sum(num1, num2) {
+  return num1 + num2;
 }
 
-const cachedData = cacheAbility()
-
-
-
+const cachedData = cacheAbility();
